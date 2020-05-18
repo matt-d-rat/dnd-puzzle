257456d6501eebfa8505d213f91f5aeb917293b2
Matt Fairbrass <matt.fairbrass@gmail.com>
2020-05-18 18:29:39 +1000

    Updates

---
+++ precache-manifest.c4c332c3066fb6f706656ac21f103823.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19da77799cf7489a9760539ba464a84",
+    "url": "/dnd-puzzle/index.html"
+  },
+  {
+    "revision": "b140bfeaff35ddb046fa",
+    "url": "/dnd-puzzle/static/css/main.77ff8be0.chunk.css"
+  },
+  {
+    "revision": "f2622bdc477fd87d64de",
+    "url": "/dnd-puzzle/static/js/2.c7f05666.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dnd-puzzle/static/js/2.c7f05666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b140bfeaff35ddb046fa",
+    "url": "/dnd-puzzle/static/js/main.7dc02606.chunk.js"
+  },
+  {
+    "revision": "2624a9c99efff3e85f64",
+    "url": "/dnd-puzzle/static/js/runtime-main.9a85249c.js"
+  },
+  {
+    "revision": "f0eafb989b3dc4a8236acf89799ca720",
+    "url": "/dnd-puzzle/static/media/icon-orientation.f0eafb98.svg"
+  },
+  {
+    "revision": "0c901dec18e33678bfde0b76c4dead26",
+    "url": "/dnd-puzzle/static/media/sprite-lever.0c901dec.png"
+  },
+  {
+    "revision": "8684cd9c76318bcf663ff1a705932635",
+    "url": "/dnd-puzzle/static/media/tile-lava.8684cd9c.gif"
+  }
+]);
